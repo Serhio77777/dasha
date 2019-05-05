09404617666573668ed6ae67cf3014cfc3d5524a
Sergey_Tkachuk <sergey.v.tkachyk@gmail.com>
2019-05-05 21:11:24 +0300

    change config for base

--- app/config/config.js
+++ app/config/config.js
@@ -3,12 +3,23 @@ const config = {
       port: process.env.PORT || 27182
     },
     sqlDB:  {
-      host: 'localhost',
-      user: 'salat',
-      password: 'dambldor',
-      database: 'tourism'
+      host: 'us-cdbr-iron-east-02.cleardb.net',
+      // host: 'mysql://b157485e0db97e:5ad60993@us-cdbr-iron-east-02.cleardb.net/heroku_fcb1b4e6abecbe4?reconnect=true',
+      user: 'ba00ce890dedc6',
+      password: 'b6702ab6',
+      database: 'heroku_b8e13114a25c918'
     }
   }
   
+// CREATE TABLE IF NOT EXISTS User (id int(11) NOT NULL AUTO_INCREMENT, role varchar(15) DEFAULT NULL, email varchar(100) DEFAULT NULL, password varchar(200) DEFAULT NULL, firstName varchar(100) DEFAULT NULL, surName varchar(100) DEFAULT NULL, image varchar(200) DEFAULT NULL, userHash varchar(100) NOT NULL UNIQUE, apiKey varhcar(100) DEFAULT NULL, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Trip (id int(11) NOT NULL AUTO_INCREMENT, name varchar(100) DEFAULT NULL, description text DEFAULT NULL, userId int(11) DEFAULT NULL, places text, PRIMARY KEY(id));
+
+// CREATE TABLE IF NOT EXISTS Tip (id int(11) NOT NULL AUTO_INCREMENT, cityId int(11) DEFAULT NULL, tips text, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Place (id int(11) NOT NULL AUTO_INCREMENT, cityId int(11) DEFAULT NULL, name varchar(100) DEFAULT NULL, description text DEFAULT NULL, places text, images text, coords text, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Image (id int(11) NOT NULL AUTO_INCREMENT, image longtext, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Discount (id int(11) NOT NULL AUTO_INCREMENT, cityId int(11) DEFAULT NULL, image varchar(100) DEFAULT NULL, name varchar(100) DEFAULT NULL, description text DEFAULT NULL, site text DEFAULT NULL, companyId int(11) DEFAULT NULL, places text, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Country (id int(11) NOT NULL AUTO_INCREMENT, name varchar(100), PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS City (id int(11) NOT NULL AUTO_INCREMENT, name varchar(100), countryId int(11) DEFAULT NULL, PRIMARY KEY(id));
+// CREATE TABLE IF NOT EXISTS Company (id int(11) NOT NULL AUTO_INCREMENT, name varchar(100) DEFAULT NULL, image varchar(100) DEFAULT NULL, discount text, PRIMARY KEY(id));
   module.exports = config
   
